2932828bdf3b3efe19ba537f64b77e1a1accc0e0
Bradley Walters <zerfgog202@gmail.com>
2017-04-09 17:43:42 -0600

    Update things

--- js/serverless-webrtc.js
+++ js/serverless-webrtc.js
@@ -1,315 +1,226 @@
-/* See also:
-    http://www.html5rocks.com/en/tutorials/webrtc/basics/
-    https://code.google.com/p/webrtc-samples/source/browse/trunk/apprtc/index.html
+let App = {
+  get activePage() {
+    return this._activePage
+  },
 
-    https://webrtc-demos.appspot.com/html/pc1.html
-*/
+  set activePage(newValue) {
+    if (this._activePage) {
+      this._activePage.classList.add("hidden")
+    }
 
-var cfg = {'iceServers': [{'url': 'stun:23.21.150.121'}]},
-  con = { 'optional': [{'DtlsSrtpKeyAgreement': true}] }
+    newValue.classList.remove("hidden")
+    this._activePage = newValue
+  }
+}
 
-/* THIS IS ALICE, THE CALLER/SENDER */
+App.router = (new Router({
+  "/home": function() {
+    App.activePage = document.querySelector("#createOrJoinPage")
+  },
 
-var pc1 = new RTCPeerConnection(cfg, con),
-  dc1 = null, tn1 = null
+  "/create-room": function() {
+    App.activePage = document.querySelector("#showLocalOfferPage")
+  },
+
+  "/create-room/await-answer": function() {
+    App.activePage = document.querySelector("#getRemoteAnswerPage")
+  },
+
+  "/join-room": function() {
+    App.activePage = document.querySelector("#getRemoteOfferPage")
+  },
+
+  "/join-room/send-answer": function() {
+    App.activePage = document.querySelector("#showLocalAnswerPage")
+  },
+
+  "/await-connection": function() {
+    App.activePage = document.querySelector("#waitForConnectionPage")
+  },
+
+  "/game": function() {
+    App.activePage = document.querySelector("#gamePage")
+  }
+})).init("/#/home")
 
 // Since the same JS file contains code for both sides of the connection,
-// activedc tracks which of the two possible datachannel variables we're using.
-var activedc
+// activedc tracks which of the two possible datachannel variables we"re using.
+let activedc
 
-var pc1icedone = false
+let cfg = { "iceServers": [{ "urls": ["stun:23.21.150.121"] }] }
+let con = { "optional": [{ "DtlsSrtpKeyAgreement": true }] }
 
-var sdpConstraints = {
-  optional: [],
-  mandatory: {
-    OfferToReceiveAudio: true,
-    OfferToReceiveVideo: true
-  }
-}
+// THIS IS ALICE, THE CALLER/SENDER
 
-$('#showLocalOffer').modal('hide')
-$('#getRemoteAnswer').modal('hide')
-$('#waitForConnection').modal('hide')
-$('#createOrJoin').modal('show')
+let alice = {
+  pc: new RTCPeerConnection(cfg, con),
+  dc: null,
 
-$('#createBtn').click(function () {
-  $('#showLocalOffer').modal('show')
-  createLocalOffer()
-})
+  setupDC() {
+    try {
+      alice.dc = alice.pc.createDataChannel("test", {reliable: true})
+      activedc = alice.dc
+      console.log("Created datachannel (pc1)")
+      alice.dc.onopen = function() {
+        console.log("data channel connect")
+        App.router.setRoute("/game")
+      }
+      alice.dc.onmessage = function(e) {
+        console.log("Got message (pc1)", e.data)
 
-$('#joinBtn').click(function () {
-  navigator.getUserMedia = navigator.getUserMedia ||
-                           navigator.webkitGetUserMedia ||
-                           navigator.mozGetUserMedia ||
-                           navigator.msGetUserMedia
-  navigator.getUserMedia({video: true, audio: true}, function (stream) {
-    var video = document.getElementById('localVideo')
-    video.src = window.URL.createObjectURL(stream)
-    video.play()
-    pc2.addStream(stream)
-  }, function (error) {
-    console.log('Error adding stream to pc2: ' + error)
-  })
-  $('#getRemoteOffer').modal('show')
-})
-
-$('#offerSentBtn').click(function () {
-  $('#getRemoteAnswer').modal('show')
-})
-
-$('#offerRecdBtn').click(function () {
-  var offer = $('#remoteOffer').val()
-  var offerDesc = new RTCSessionDescription(JSON.parse(offer))
-  console.log('Received remote offer', offerDesc)
-  writeToChatLog('Received remote offer', 'text-success')
-  handleOfferFromPC1(offerDesc)
-  $('#showLocalAnswer').modal('show')
-})
-
-$('#answerSentBtn').click(function () {
-  $('#waitForConnection').modal('show')
-})
-
-$('#answerRecdBtn').click(function () {
-  var answer = $('#remoteAnswer').val()
-  var answerDesc = new RTCSessionDescription(JSON.parse(answer))
-  handleAnswerFromPC2(answerDesc)
-  $('#waitForConnection').modal('show')
-})
-
-$('#fileBtn').change(function () {
-  var file = this.files[0]
-  console.log(file)
-
-  sendFile(file)
-})
-
-function fileSent (file) {
-  console.log(file + ' sent')
-}
-
-function fileProgress (file) {
-  console.log(file + ' progress')
-}
-
-function sendFile (data) {
-  if (data.size) {
-    FileSender.send({
-      file: data,
-      onFileSent: fileSent,
-      onFileProgress: fileProgress,
-    })
-  }
-}
-
-function sendMessage () {
-  if ($('#messageTextBox').val()) {
-    var channel = new RTCMultiSession()
-    writeToChatLog($('#messageTextBox').val(), 'text-success')
-    channel.send({message: $('#messageTextBox').val()})
-    $('#messageTextBox').val('')
-
-    // Scroll chat text area to the bottom on new input.
-    $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight)
-  }
-
-  return false
-}
-
-function setupDC1 () {
-  try {
-    var fileReceiver1 = new FileReceiver()
-    dc1 = pc1.createDataChannel('test', {reliable: true})
-    activedc = dc1
-    console.log('Created datachannel (pc1)')
-    dc1.onopen = function (e) {
-      console.log('data channel connect')
-      $('#waitForConnection').modal('hide')
-      $('#waitForConnection').remove()
-    }
-    dc1.onmessage = function (e) {
-      console.log('Got message (pc1)', e.data)
-      if (e.data.size) {
-        fileReceiver1.receive(e.data, {})
-      } else {
         if (e.data.charCodeAt(0) == 2) {
           // The first message we get from Firefox (but not Chrome)
-          // is literal ASCII 2 and I don't understand why -- if we
+          // is literal ASCII 2 and I do not understand why -- if we
           // leave it in, JSON.parse() will barf.
           return
         }
         console.log(e)
-        var data = JSON.parse(e.data)
-        if (data.type === 'file') {
-          fileReceiver1.receive(e.data, {})
-        } else {
-          writeToChatLog(data.message, 'text-info')
-          // Scroll chat text area to the bottom on new input.
-          $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight)
-        }
+
+        let data = JSON.parse(e.data)
+
+        writeToChatLog(data.message, "text-info")
       }
-    }
-  } catch (e) { console.warn('No data channel (pc1)', e); }
-}
-
-function createLocalOffer () {
-  console.log('video1')
-  navigator.getUserMedia = navigator.getUserMedia ||
-                           navigator.webkitGetUserMedia ||
-                           navigator.mozGetUserMedia ||
-                           navigator.msGetUserMedia
-  navigator.getUserMedia({video: true, audio: true}, function (stream) {
-    var video = document.getElementById('localVideo')
-    video.src = window.URL.createObjectURL(stream)
-    video.play()
-    pc1.addStream(stream)
-    console.log(stream)
-    console.log('adding stream to pc1')
-    setupDC1()
-    pc1.createOffer(function (desc) {
-      pc1.setLocalDescription(desc, function () {}, function () {})
-      console.log('created local offer', desc)
-    },
-    function () { console.warn("Couldn't create offer") },
-    sdpConstraints)
-  }, function (error) {
-    console.log('Error adding stream to pc1: ' + error)
-  })
-}
-
-pc1.onicecandidate = function (e) {
-  console.log('ICE candidate (pc1)', e)
-  if (e.candidate == null) {
-    $('#localOffer').html(JSON.stringify(pc1.localDescription))
-  }
-}
-
-function handleOnaddstream (e) {
-  console.log('Got remote stream', e.stream)
-  var el = document.getElementById('remoteVideo')
-  el.autoplay = true
-  attachMediaStream(el, e.stream)
-}
-
-pc1.onaddstream = handleOnaddstream
-
-function handleOnconnection () {
-  console.log('Datachannel connected')
-  writeToChatLog('Datachannel connected', 'text-success')
-  $('#waitForConnection').modal('hide')
-  // If we didn't call remove() here, there would be a race on pc2:
-  //   - first onconnection() hides the dialog, then someone clicks
-  //     on answerSentBtn which shows it, and it stays shown forever.
-  $('#waitForConnection').remove()
-  $('#showLocalAnswer').modal('hide')
-  $('#messageTextBox').focus()
-}
-
-pc1.onconnection = handleOnconnection
-
-function onsignalingstatechange (state) {
-  console.info('signaling state change:', state)
-}
-
-function oniceconnectionstatechange (state) {
-  console.info('ice connection state change:', state)
-}
-
-function onicegatheringstatechange (state) {
-  console.info('ice gathering state change:', state)
-}
-
-pc1.onsignalingstatechange = onsignalingstatechange
-pc1.oniceconnectionstatechange = oniceconnectionstatechange
-pc1.onicegatheringstatechange = onicegatheringstatechange
-
-function handleAnswerFromPC2 (answerDesc) {
-  console.log('Received remote answer: ', answerDesc)
-  writeToChatLog('Received remote answer', 'text-success')
-  pc1.setRemoteDescription(answerDesc)
-}
-
-function handleCandidateFromPC2 (iceCandidate) {
-  pc1.addIceCandidate(iceCandidate)
-}
-
-/* THIS IS BOB, THE ANSWERER/RECEIVER */
-
-var pc2 = new RTCPeerConnection(cfg, con),
-  dc2 = null
-
-var pc2icedone = false
-
-pc2.ondatachannel = function (e) {
-  var fileReceiver2 = new FileReceiver()
-  var datachannel = e.channel || e; // Chrome sends event, FF sends raw channel
-  console.log('Received datachannel (pc2)', arguments)
-  dc2 = datachannel
-  activedc = dc2
-  dc2.onopen = function (e) {
-    console.log('data channel connect')
-    $('#waitForConnection').modal('hide')
-    $('#waitForConnection').remove()
-  }
-  dc2.onmessage = function (e) {
-    console.log('Got message (pc2)', e.data)
-    if (e.data.size) {
-      fileReceiver2.receive(e.data, {})
-    } else {
-      var data = JSON.parse(e.data)
-      if (data.type === 'file') {
-        fileReceiver2.receive(e.data, {})
-      } else {
-        writeToChatLog(data.message, 'text-info')
-        // Scroll chat text area to the bottom on new input.
-        $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight)
-      }
+    } catch (e) {
+      console.warn("No data channel (pc1)", e);
     }
   }
 }
 
-function handleOfferFromPC1 (offerDesc) {
-  pc2.setRemoteDescription(offerDesc)
-  pc2.createAnswer(function (answerDesc) {
-    writeToChatLog('Created local answer', 'text-success')
-    console.log('Created local answer: ', answerDesc)
-    pc2.setLocalDescription(answerDesc)
+let sdpConstraints = {
+  optional: [],
+}
+
+document.querySelector("#createRoomButton").addEventListener("click", function() {
+  App.router.setRoute("/create-room")
+
+  console.log("createLocalOffer")
+
+  alice.setupDC()
+
+  alice.pc.createOffer(function(desc) {
+    alice.pc.setLocalDescription(desc, function() {}, function() {})
+    console.log("created local offer", desc)
+  }, function() {
+    console.warn("Couldn't create offer")
+  }, sdpConstraints)
+})
+
+document.querySelector("#remoteOfferReceivedButton").addEventListener("click", function() {
+  let remoteOfferInput = document.querySelector("#remoteOfferInput")
+  let offer = remoteOfferInput.value
+  remoteOfferInput.value = ""
+
+  let offerDesc = new RTCSessionDescription(JSON.parse(offer))
+
+  console.log("Received remote offer", offerDesc)
+  writeToChatLog("Received remote offer", "text-success")
+
+  // handle the offer from alice.pc
+  bob.pc.setRemoteDescription(offerDesc)
+  bob.pc.createAnswer(function(answerDesc) {
+    writeToChatLog("Created local answer", "text-success")
+    console.log("Created local answer: ", answerDesc)
+    bob.pc.setLocalDescription(answerDesc)
   },
-  function () { console.warn("Couldn't create offer") },
+  function() { console.warn("Could not create offer") },
   sdpConstraints)
+
+  App.router.setRoute("/join-room/send-answer")
+})
+
+let writeToChatLog = function(message, message_type) {
+  let p = document.createElement("p")
+  p.classList.add(message_type)
+  p.innerText = `[${(new Date()).toLocaleTimeString()}] ${message}`
+  document.querySelector("#chatlog").appendChild(p)
+  document.querySelector("#chatlog").scrollTop = document.querySelector("#chatlog").scrollHeight
 }
 
-pc2.onicecandidate = function (e) {
-  console.log('ICE candidate (pc2)', e)
+document.querySelector("#answerReceivedButton").addEventListener("click", function() {
+  let remoteAnswerInput = document.querySelector("#remoteAnswerInput")
+  let answer = remoteAnswerInput.value
+  remoteAnswerInput.value = ""
+
+  let answerDesc = new RTCSessionDescription(JSON.parse(answer))
+
+  // handle answer from bob.pc
+  console.log("Received remote answer: ", answerDesc)
+  writeToChatLog("Received remote answer", "text-success")
+  alice.pc.setRemoteDescription(answerDesc)
+
+  App.router.setRoute("/await-connection")
+})
+
+document.querySelector("#sendMessageButton").addEventListener("click", function() {
+  if (document.querySelector("#messageTextBox").value) {
+    writeToChatLog(document.querySelector("#messageTextBox").value, "text-success")
+
+    activedc.send(JSON.stringify({ "message": document.querySelector("#messageTextBox").value }))
+
+    document.querySelector("#messageTextBox").value = ""
+  }
+
+  return false
+})
+
+alice.pc.onicecandidate = function(e) {
+  console.log("ICE candidate (alice.pc)", e)
   if (e.candidate == null) {
-    $('#localAnswer').html(JSON.stringify(pc2.localDescription))
+    document.querySelector("#localOfferText").innerText = JSON.stringify(alice.pc.localDescription)
   }
 }
 
-pc2.onsignalingstatechange = onsignalingstatechange
-pc2.oniceconnectionstatechange = oniceconnectionstatechange
-pc2.onicegatheringstatechange = onicegatheringstatechange
-
-function handleCandidateFromPC1 (iceCandidate) {
-  pc2.addIceCandidate(iceCandidate)
+alice.pc.onaddstream = function(e) {
+  console.log("Got remote stream", e.stream)
 }
 
-pc2.onaddstream = handleOnaddstream
-pc2.onconnection = handleOnconnection
-
-function getTimestamp () {
-  var totalSec = new Date().getTime() / 1000
-  var hours = parseInt(totalSec / 3600) % 24
-  var minutes = parseInt(totalSec / 60) % 60
-  var seconds = parseInt(totalSec % 60)
-
-  var result = (hours < 10 ? '0' + hours : hours) + ':' +
-    (minutes < 10 ? '0' + minutes : minutes) + ':' +
-    (seconds < 10 ? '0' + seconds : seconds)
-
-  return result
+function handleOnconnection() {
+  console.log("Datachannel connected")
+  writeToChatLog("Datachannel connected", "text-success")
+  App.router.setRoute("/game")
 }
 
-function writeToChatLog (message, message_type) {
-  document.getElementById('chatlog').innerHTML += '<p class="' + message_type + '">' + '[' + getTimestamp() + '] ' + message + '</p>'
+alice.pc.onconnection = handleOnconnection
+
+alice.pc.onsignalingstatechange = (state) => console.info("signaling state change:", state)
+alice.pc.oniceconnectionstatechange = (state) => console.info("ice connection state change:", state)
+alice.pc.onicegatheringstatechange = (state) => console.info("ice gathering state change:", state)
+
+// THIS IS BOB, THE ANSWERER/RECEIVER
+
+let bob = {
+  pc: new RTCPeerConnection(cfg, con),
+  dc: null
 }
+
+bob.pc.ondatachannel = function(e) {
+  let datachannel = e.channel || e; // Chrome sends event, FF sends raw channel
+  console.log("Received datachannel (bob.pc)", arguments)
+  bob.dc = datachannel
+  activedc = bob.dc
+  bob.dc.onopen = function() {
+    console.log("data channel connect")
+    App.router.setRoute("/game")
+  }
+  bob.dc.onmessage = function(e) {
+    console.log("Got message (bob.pc)", e.data)
+
+    let data = JSON.parse(e.data)
+    writeToChatLog(data.message, "text-info")
+  }
+}
+
+bob.pc.onicecandidate = function(e) {
+  console.log("ICE candidate (bob.pc)", e)
+  if (e.candidate == null) {
+    document.querySelector("#localAnswerText").innerText = JSON.stringify(bob.pc.localDescription)
+  }
+}
+
+bob.pc.onsignalingstatechange = (state) => console.info("signaling state change:", state)
+bob.pc.oniceconnectionstatechange = (state) => console.info("ice connection state change:", state)
+bob.pc.onicegatheringstatechange = (state) => console.info("ice gathering state change:", state)
+
+bob.pc.onaddstream = (e) => console.log("Got remote stream", e.stream)
+
+bob.pc.onconnection = handleOnconnection
